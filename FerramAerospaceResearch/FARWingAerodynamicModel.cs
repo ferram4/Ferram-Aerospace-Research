3d3a55d86ae799a8eb173050f63410d9426b0d20
ferram4 <ferram40@gmail.com>
2014-10-15 01:31:19 -0400

    Some cleanup of debug values

--- FerramAerospaceResearch/FARWingAerodynamicModel.cs
+++ FerramAerospaceResearch/FARWingAerodynamicModel.cs
@@ -54,9 +54,6 @@ namespace ferram4
         [KSPField(isPersistant = false, guiActive = false, guiActiveEditor = true)]
         public float curWingMass = 1;
 
-        [KSPField(isPersistant = false, guiActive = true)]
-        protected double effectiveInfluence;
-
         [KSPField(isPersistant = true, guiActive = false, guiActiveEditor = true, guiName = "Mass/Strength Multiplier", guiFormat = "0.##"), UI_FloatRange(minValue = 0.1f, maxValue = 2.0f, stepIncrement = 0.01f)]
         public float massMultiplier = 1.0f;
 
@@ -617,7 +614,6 @@ namespace ferram4
 
                 effectiveUpstreamInfluence = wingInteraction.EffectiveUpstreamInfluence;
 
-                effectiveInfluence = effectiveUpstreamInfluence;
                 AoAmax = wingInteraction.EffectiveUpstreamAoAMax;
                 liftslope *= (1 - effectiveUpstreamInfluence);
                 liftslope += wingInteraction.EffectiveUpstreamLiftSlope;
