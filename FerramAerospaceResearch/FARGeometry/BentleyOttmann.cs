c609922b0e2b3517b24a1d3fb8b29a345bd0827f
ferram4 <ferram40@gmail.com>
2014-11-20 22:31:12 -0500

    More Bentley

--- FerramAerospaceResearch/FARGeometry/BentleyOttmann.cs
+++ FerramAerospaceResearch/FARGeometry/BentleyOttmann.cs
@@ -10,5 +10,19 @@ namespace FerramAerospaceResearch.FARGeometry
     {
         private LLRedBlackTree<FARGeometryLineSegment> sweepLine;
         private BentleyOttmannEventQueue eventQueue;
+
+
+        /// <summary>
+        /// This merges two polygons using the Bentley-Ottmann Algorithm
+        /// </summary>
+        /// <param name="poly1">First poly; this will be used for unified transforms and will contain the output of this method</param>
+        /// <param name="poly2">Poly to be merged</param>
+        public void MergePolygons(ref FARGeometryPartPolygon poly1, FARGeometryPartPolygon poly2)
+        {
+            List<FARGeometryLineSegment> lines = new List<FARGeometryLineSegment>(poly1.PlanformBoundsLines);
+            lines.AddRange(poly2.PlanformBoundsLines);
+
+            eventQueue = new BentleyOttmannEventQueue(lines);
+        }
     }
 }
