02e3690f27f92a2627dc4fa4c61eabf2a5318eb1
stupid_chris <christophe_savard@hotmail.ca>
2015-04-28 01:53:47 -0400

    Fix .csproj compile list

--- FerramAerospaceResearch/RealChuteLite/RealChuteFAR.cs
+++ FerramAerospaceResearch/RealChuteLite/RealChuteFAR.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
-using ferram4;
 using Random = System.Random;
 
 namespace FerramAerospaceResearch.RealChuteLite
