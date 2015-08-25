9c466fc25d92f17cc6e6c01ecd3a019a08f834bd
ferram4 <ferram40@gmail.com>
2015-08-25 08:41:04 -0400

    Update version and name in source

--- FerramAerospaceResearch/FARGUI/FARFlightGUI/FlightGUI.cs
+++ FerramAerospaceResearch/FARGUI/FARFlightGUI/FlightGUI.cs
@@ -1,5 +1,5 @@
 ﻿/*
-Ferram Aerospace Research v0.15.4.1 "Goldstein"
+Ferram Aerospace Research v0.15.5 "Haack"
 =========================
 Aerodynamics model for Kerbal Space Program
 
@@ -267,7 +267,7 @@ namespace FerramAerospaceResearch.FARGUI.FARFlightGUI
             }
             if (_vessel == FlightGlobals.ActiveVessel && showGUI && showAllGUI)
             {
-                mainGuiRect = GUILayout.Window(this.GetHashCode(), mainGuiRect, MainFlightGUIWindow, "FAR, v0.15.4.1 'Goldstein'", GUILayout.MinWidth(230));
+                mainGuiRect = GUILayout.Window(this.GetHashCode(), mainGuiRect, MainFlightGUIWindow, "FAR, v0.15.5 'Haack'", GUILayout.MinWidth(230));
                 GUIUtils.ClampToScreen(mainGuiRect);
 
                 if (showFlightDataWindow)
